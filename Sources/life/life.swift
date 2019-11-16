aa56f501a69d61a4c03695726d45aa96387c1636
Matthew Healy <matthew.healy@soundcloud.com>
2019-11-16 14:24:36 +0100

    Implement initial rules for cell (0, 0)

--- Sources/life/life.swift
+++ Sources/life/life.swift
@@ -1,3 +1,33 @@
-struct life {
-    var text = "Hello, World!"
+public struct Game {
+    private var liveCells: Set<Coordinate> = []
+    private var cellState: CellState = .dead
+
+    public init() {}
+
+    public mutating func setLive(at coordinate: Coordinate) {
+        liveCells.insert(coordinate)
+    }
+
+    public mutating func nextGeneration() {
+        let shouldBeRevived = liveCells.count == 2 || liveCells.count == 3
+        cellState = shouldBeRevived ? .live : .dead
+    }
+
+    public func state(atCoordinate: (x: Int, y: Int)) -> CellState {
+        return cellState
+    }
+}
+
+public struct Coordinate: Hashable {
+    let x: Int
+    let y: Int
+
+    public init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+public enum CellState {
+    case dead, live
 }
--- Tests/lifeTests/lifeTests.swift
+++ Tests/lifeTests/lifeTests.swift
@@ -1,15 +1,45 @@
 import XCTest
-@testable import life
+import life
 
 final class lifeTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(life().text, "Hello, World!")
+    static var allTests = [
+        ("Live cell with no neighbours dies", test_whenLiveCellHasNoNeighbours_itDies),
+        ("Dead cell with 2 neighbours revives", test_whenDeadCellHasTwoNeighbours_itRevives),
+        ("Dead cell with 3 neighbours revives", test_whenDeadCellHasThreeNeighbours_itRevives)
+    ]
+
+    private var game: Game!
+
+    override func setUp() {
+        super.setUp()
+        game = Game()
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    override func tearDown() {
+        game = nil
+        super.tearDown()
+    }
+
+    func test_whenLiveCellHasNoNeighbours_itDies() {
+        setLive(at: [(0, 0)])
+        game.nextGeneration()
+        XCTAssertEqual(.dead, game.state(atCoordinate: (x: 0, y: 0)))
+    }
+
+    func test_whenDeadCellHasTwoNeighbours_itRevives() {
+        setLive(at: [(0, 1), (1, 0)])
+        game.nextGeneration()
+        XCTAssertEqual(.live, game.state(atCoordinate: (x: 0, y: 0)))
+    }
+
+    func test_whenDeadCellHasThreeNeighbours_itRevives() {
+        setLive(at: [(0, 1), (1, 1), (1, 0)])
+        game.nextGeneration()
+        XCTAssertEqual(.live, game.state(atCoordinate: (x: 0, y: 0)))
+    }
+
+    private func setLive(at coordinates: [(Int, Int)]) {
+        coordinates.map(Coordinate.init)
+            .forEach { game.setLive(at: $0) }
+    }
 }
